bcf443d35ec43050b26736a7545d17bcf6e1d2e6
andeki92 <anders@bacheklever.no>
2025-04-28 12:34:54 +0200

    fix

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -45,7 +45,7 @@ setup_branch_protection() {
   "required_status_checks": {
     "strict": true,
     "contexts": [
-      "Zsh Startup Benchmark / All Green"
+      "All Green Benchmark"
     ]
   },
   "enforce_admins": false,
